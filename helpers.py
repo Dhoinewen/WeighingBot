42007bf0571c1981268a3f5711db1b79fea54ca8
Maxim <pogchamp1401@gmail.com>
2022-06-13 23:55:01 +0300

    322

--- helpers.py
+++ helpers.py
@@ -1,13 +1,17 @@
+from model import session, Weight
+from datetime import datetime
+
+
 def read_token(file_name):
     with open(file_name, 'r') as file:
         token = file.readline()
     return token
 
 
-def from_oject_to_str(data):
+def from_object_to_str(data):
     text = ''
     for data in data.weights:
-        text += str(data) + ' kg ' + data.created_at.strftime("%Y-%m-%d %H:%M") + '\n'
+        text += str(data) + ' kg ' + data.created_at.strftime("<u>%Y-%m-%d</u>") + '\n'
     return text
 
 
@@ -20,3 +24,11 @@ def check_weight(message_weight):
     if float_message > 130 or float_message < 40:
         return False
     return float_message
+
+
+def weighing_was_today():
+    last_weight = session.query(Weight).order_by(Weight.id.desc()).first()
+    if datetime.now().date() == last_weight.created_at.date():
+        return False
+    else:
+        return True
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from helpers import from_oject_to_str, check_weight, read_token
+from helpers import from_object_to_str, check_weight, read_token, weighing_was_today
 from data import create_user, create_weighing, get_all_weighing
 
 
@@ -14,7 +14,7 @@ API_TOKEN = read_token('bot_token.txt')
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
@@ -25,7 +25,7 @@ class FSMText(StatesGroup):
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = ["Add waighing", "Show my stats"]
+    buttons = ["Add weighing", "Show my stats", "Stop"]
     keyboard.add(*buttons)
     registered = create_user(message.from_user.id, message.from_user.username)
     if registered is False:
@@ -34,18 +34,28 @@ async def send_welcome(message: types.Message):
         await message.answer("Successfully Registered", reply_markup=keyboard)
 
 
-@dp.message_handler(lambda message: message.text == "Add waighing", state=None)
+@dp.message_handler(lambda message: message.text == "Add weighing", state='*')
 async def add_weighing(message: types.Message):
-    await message.answer('Вкажіть вагу')
+    if weighing_was_today() is False:
+        await message.answer('Only 1 weighing per day')
+        return
+    await message.answer('Enter weight')
     await FSMText.weight.set()
+
+
+@dp.message_handler(lambda message: message.text == "Stop", state='*')
+async def cmd_cancel(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer("Cancel")
 
 
 @dp.message_handler(state=FSMText.weight)
 async def save_weighing(message: types.Message, state: FSMContext):
     weight_after_check = check_weight(message.text)
     if weight_after_check is False:
-        await message.reply('Неправильна вага, введіть повторно')
+        await message.reply('Invalid format, re-enter data')
         return
+    await state.update_data(enter_weight=weight_after_check)
     create_weighing(message.from_user.id, weight_after_check)
     await state.finish()
     await message.answer('Saved')
@@ -54,7 +64,8 @@ async def save_weighing(message: types.Message, state: FSMContext):
 @dp.message_handler(lambda message: message.text == "Show my stats")
 async def show_my_stats(message: types.Message):
     data = get_all_weighing(message.from_user.id)
-    await message.answer(from_oject_to_str(data))
+    await message.answer(from_object_to_str(data))
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
